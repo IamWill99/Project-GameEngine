823c9fa01f97e5ef6f521eba1a70360793a9e27e
Timor Yuksel <timor.yuksel@hva.nl>
2024-03-06 15:28:19 +0100

    Thebigroom

--- src/api/src/rooms/Thebigroom.ts
+++ src/api/src/rooms/Thebigroom.ts
@@ -1,5 +1,8 @@
 import { ActionResult } from "../base/actionResults/ActionResult";
 import { TextActionResult } from "../base/actionResults/TextActionResult";
+import { Action } from "../base/actions/Action";
+import { CustomAction } from "../base/actions/CustomAction";
+import { ExamineAction } from "../base/actions/ExamineAction";
 import { GameObject } from "../base/gameObjects/GameObject";
 import { Room } from "../base/gameObjects/Room";
 
@@ -15,18 +18,31 @@ export class Thebigroom extends Room{
         return "The big room";
     }
 
+    public images(): string[] {
+        return [
+            "thebigroom"
+        ];
+    }
+
+    public actions(): Action[] {
+        return [new ExamineAction(), new CustomAction("test me", "test", false)];
+    }
+
+    public objects(): GameObject[] {
+        return [this];
+    }
+
     public examine(): ActionResult | undefined {
         return new TextActionResult(["Grote kamer!","Donker."]);
     }
 
-    public images(): string[] {
-        return [
-            "Thebigroom"
-        ];
-    }
+    public custom(alias: string, _gameObjects: GameObject[] | undefined): ActionResult | undefined {
+        if(alias === "test me"){
+            return new TextActionResult(["Je schreeuwt in de kamer en je hoort een echo"]); 
 
-    public objects(): GameObject[] {
-        return [];
+        }
+
+        return undefined;
     }
 
 
